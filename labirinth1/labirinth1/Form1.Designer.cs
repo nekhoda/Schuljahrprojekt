0ac7e45b045db85173066ad1ce9bde10e92a0297
Veniamin <venyanehoda@gmail.com>
2022-05-26 21:40:20 +0300

    release v1

---
+++ labirinth1/labirinth1/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace labirinth1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label9.Location = new System.Drawing.Point(900, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(164, 13);
+            this.label9.TabIndex = 43;
+            this.label9.Text = "Найкоротший шлях з лабіринту";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(906, 79);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 472);
+            this.listBox2.TabIndex = 42;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label8.Location = new System.Drawing.Point(777, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Шляхи з лабіринту";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(780, 79);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 472);
+            this.listBox1.TabIndex = 40;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(797, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(242, 50);
+            this.button4.TabIndex = 39;
+            this.button4.Text = "ВИЙТИ З ПРОГРАМИ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.Location = new System.Drawing.Point(670, 442);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "ЧИ Є ВИХІД?";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(503, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(288, 50);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "ШУКАТИ ВИХІД ІЗ ДОВІЛЬНОЇ ТОЧКИ ЛАБІРИНТУ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(762, 374);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 50);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "СТВОРИТИ ЛАБИРИНТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(299, 50);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "ШУКАТИ ВИХІД ІЗ ПОЧАТКУ ЛАБІРИНТУ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 500);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 45;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 500);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 46;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(1097, 79);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 472);
+            this.listBox3.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1094, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(1094, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Найдовший шлях з лабіринту";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(24, 461);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(498, 13);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "КООРДИНАТИ ДОВІЛЬНОЇ ТОЧКИ(будьте уважними довільна точка повинна бути проходом (" +
+    "1))";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Location = new System.Drawing.Point(24, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "ПЕРША КООРДИНАТА (Х)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Location = new System.Drawing.Point(186, 484);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 13);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "ДРУГА КООРДИНАТА (Y)";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(1266, 79);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(209, 472);
+            this.listBox4.TabIndex = 53;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.Location = new System.Drawing.Point(1263, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 13);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "Інші відомості про лабіринт";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1495, 596);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Лабіринт";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ labirinth1/labirinth1/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace labirinth1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] a = new int[11, 11];//Масив а відтворює лабіринт
+        String labirint = "labirint.txt";//Файл, що містить цей масив.
+        String results = "results.txt";//Файл для збереження результатів
+        /* {
+           { 0,0,0,0,0,0,0,0,0,0,0},
+           { 0,0,1,0,0,0,0,0,0,0,0},
+           { 0,1,1,0,1,0,0,1,1,1,0},
+           { 0,0,1,1,1,1,1,1,0,1,0},
+           { 0,0,0,0,1,0,1,0,0,1,0},
+           { 0,0,1,0,1,1,1,1,0,1,0},
+           { 0,0,1,1,1,0,0,0,0,1,0},
+           { 0,0,0,0,1,1,0,1,0,1,0},
+           { 0,0,1,0,0,1,1,1,0,1,0},
+           { 0,0,1,1,1,1,0,0,1,1,0},
+           { 0,0,0,0,0,0,0,0,0,0,0},
+       };Заповнюємо лабірнт стінами і проходами, де:
+                     * "0" - це стіна;
+                     * "1" - це прохід;
+                     */
+        int i1;
+        int j1;//i1,j1-координати входу.
+        int i2 = 8;
+        int j2 = 9;//i2,j2-координати виходу.
+        int c = 0;//Лічильник кроків.
+        int tupik = 0;//Лічильник тупіків.
+        int way = 0;//Лічильник шляхів.
+        int minc = 99999;//Для обчислення найкоротшого шляху.
+        int maxc = -99999;//Для обчислення найдовшого шляху.
+        int[,] l;//Для запам'ятовування шляху.
+        int[,] minl;//Для запам'ятовування мінімального шляху.
+        int[,] maxl;//Для запам'ятовування максимального шляху.
+        public void find(int x, int y)
+        {
+            c++;//Зроблено крок.
+            l[c, 0] = x;
+            l[c, 1] = y;//Запам'ятовуємо координати кожного кроку, щоб потім вивести шлях.
+            if ((x == i2) && (y == j2))//Перевіряємо поточну позицію в лабіринті: якщо це вихід, виходимо, якщо ні - рухаємося далі.
+            {
+                StreamWriter w = new StreamWriter(results,true);//Створюємо засіб запису до файлу.
+                way++;
+                label7.Text = "ВИХІД Є";
+                listBox1.Items.Add("Вихід номер " + way);
+                w.WriteLine("Вихід номер " + way);
+                for (int i = 1; i <= c; i++)
+                {
+                    listBox1.Items.Add("x:" + l[i, 0] + " " + "y:" + l[i, 1]);//Виводимо по кроках шлях.
+                    w.WriteLine("x:" + l[i, 0] + " " + "y:" + l[i, 1]);
+                    dataGridView1.Rows[l[i, 1]].Cells[l[i, 0]].Style.BackColor=Color.FromArgb(200 - way * 20, 100+way*10,50 + way * 20);/*Додаємо "way",
+                    щоб у різних шляхів були різні кольори.*/
+
+                }
+                listBox4.Items.Add("Довжина шляху номер " + way+" : "+c);
+                w.WriteLine("Довжина шляху номер " + way + " : " + c);
+                w.Close();
+                if (c < minc)
+                {
+                    minc = c;
+                    for (int i = 1; i <= minc; i++)//Заповнюємо масив, де зберігається найкоротший шлях.
+                    {
+                        minl[i, 0] = l[i, 0];
+                        minl[i, 1] = l[i, 1];
+                    }
+                }
+                if (c > maxc)
+                {
+                    maxc = c;
+                    for (int i = 1; i <= maxc; i++)//Заповнюємо масив, де зберігається найдовший шлях.
+                    {
+                        maxl[i, 0] = l[i, 0];
+                        maxl[i, 1] = l[i, 1];
+                    }
+                }
+            }
+            int k = 0;//За допомогою k визначаємо, скільки навколо поточної позиції стінок. Якщо їх 3, це тупік.
+            a[x, y] = 2;//Далі ми позначаємо цю точку "2". Це значить, що ми тут вже були.
+            if (a[x + 1, y] == 1) find(x + 1, y);
+            else
+            {
+                if (a[x + 1, y] == 0) k++;
+            }
+            if (a[x - 1, y] == 1) find(x - 1, y);
+            else
+            {
+                if (a[x - 1, y] == 0) k++;
+            }
+            if (a[x, y - 1] == 1) find(x, y - 1);
+            else
+            {
+                if (a[x, y-1] == 0) k++;
+            }
+            if (a[x, y + 1] == 1) find(x, y + 1);
+            else
+            {
+                if (a[x, y+1] == 0) k++;
+            }
+            if (k == 3)
+            {
+                tupik++;
+            }
+            a[x, y] = 1;
+            c--;//віднімає крок
+        }
+        private void button1_Click_1(object sender, EventArgs e)/*Натисканням першої кнопки копіюємо лабіринт із текстового файлу до масиву "а"
+та виводимо його на екран.*/
+        {
+            i1 = 2;
+            j1 = 1;
+            int k = -1;
+            String s1;
+            StreamReader str = new StreamReader(labirint); //*Створюємо засіб читання з файлу, відкриваємо файл для читання з нього.
+            while ((s1 = str.ReadLine()) != null)//Доки не дійшли до кінця файлу, читаємо рядки з нього.
+            {
+                k++;
+               for (int i = 0; i < s1.Length; i++)
+                {
+                    a[i, k] = Convert.ToInt16(s1[i])-48;//Заповнюємо масив "а" лабирінтом.
+                }             
+            }
+            str.Close();//закриваємо наш файл
+            dataGridView1.ColumnCount = 11;//Задаємо кількість колонок.
+            dataGridView1.RowCount = 11;//Задаємо кількість стовпців.
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    dataGridView1.Rows[j].Cells[i].Value = a[i, j];//Заповнюємо  dataGridView1.
+                }
+            }
+            for (int i = 0; i < 11; i++)
+            {
+                int j = 0;
+                dataGridView1.Rows[j].Cells[i].Value = i;
+            }
+            for (int j = 0; j < 11; j++)
+            {
+                int i = 0;
+                dataGridView1.Rows[j].Cells[i].Value = j;
+            }//Нумерація рядків і стовпчиків.      
+            dataGridView1.Rows[j1].Cells[i1].Value = "S";//S- місце старту.
+            dataGridView1.Rows[j2].Cells[i2].Value = "F";//F- місце фінішу.
+        }     
+        private void button2_Click(object sender, EventArgs e)//Натиснувши другу кнопку, запускаємо рух по лабіринту.
+        {
+            dataGridView1.Rows[j1].Cells[i1].Value = "1";//Стираємо минулий початок, якщо він був.
+            i1 = 2;
+            j1 = 1;//Координати входу до лабіринту за замовчуванням.       
+           dataGridView1.Rows[j1].Cells[i1].Value = "S";
+            l = new int[11 * 11, 2];
+            minl = new int[11 * 11, 2];
+            maxl = new int[11 * 11, 2];//Заповнюємо масиви для виходів.
+            label7.Text = "ВИХОДУ НЕМАЄ";/*Спочатку пишемо що виходу немає (перед рядком із процедурою  find).
+           Якщо find знайде шлях крізь лабіринт, напис буде змінено.*/
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;//Обнуляємо замальовані комірки.
+                }
+            }
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            minc = 999999;
+            maxc = -99999;
+            c = 0;
+            tupik = 0;
+            way = 0;
+            StreamWriter nul = new StreamWriter(results);//Чистимо файл із результатами перед запуском проекту на виконання.
+            nul.Close();//Обнулюємо всі лічильники перед початком роботи процедури  find.
+            find(i1, j1);
+            StreamWriter SW = new StreamWriter(results, true);//Створюємо засіб запису до файлу.
+            SW.WriteLine("Найкоротший шлях");
+            for (int i = 1; i <= minc; i++)
+            {
+                listBox2.Items.Add("x:" + minl[i, 0] + " " + "y:" + minl[i, 1]);//Виводимо найкоротший шлях.
+                SW.WriteLine("x:" + minl[i, 0] + " " + "y:" + minl[i, 1]);
+            }
+            SW.WriteLine("Найдовший шлях");
+            for (int i = 1; i <= maxc; i++)
+            {
+                listBox3.Items.Add("x:" + maxl[i, 0] + " " + "y:" + maxl[i, 1]);//Виводимо найдовший шлях.
+                SW.WriteLine("x:" + maxl[i, 0] + " " + "y:" + maxl[i, 1]);
+            }       
+            listBox4.Items.Add("Довжина найкоротшого виходу " + minc);
+            listBox4.Items.Add("Довжина найдовшого виходу " + maxc);
+            listBox4.Items.Add("Кількість виходів у лабіринті " + way);
+            listBox4.Items.Add("Кількість тупіків у лабіринті " + tupik);
+            SW.WriteLine("Довжина найкоротшого виходу " + minc);
+            SW.WriteLine("Довжина найдовшого виходу " + maxc);
+            SW.WriteLine("Кількість виходів у лабіринті " + way);
+            SW.WriteLine("Кількість тупіків у лабіринті " + tupik);
+            SW.Close();
+        }
+        private void button3_Click_1(object sender, EventArgs e)/*Натиснувши третю кнопку, проходимо лабіринт зі стартової позиції,
+        яка може бути розташована в довільному місці лабіринту.*/
+        {
+            dataGridView1.Rows[j1].Cells[i1].Value = "1";//Стираємо минулий початок, якщо він був.
+            i1 = int.Parse(textBox1.Text);//Вводимо першу координату.
+            j1 = int.Parse(textBox2.Text);//Вводимо другу координату.
+            if (a[i1, j1] == 0)//Важливо, щоб вхід було встановлено не на стіні!
+            {
+                MessageBox.Show("Це стіна!");
+            }
+            else
+            {
+                dataGridView1.Rows[1].Cells[2].Value = 1;
+                dataGridView1.Rows[j1].Cells[i1].Value = "S";
+                l = new int[11 * 11, 2];
+                minl = new int[11 * 11, 2];
+                maxl = new int[11 * 11, 2];//заповнюємо масиви для виходів.
+                label7.Text = "ВИХОДУ НЕМАЄ";/*спочатку пишемо що виходу немає, спеціально перед рядком з процедурою  find:
+            якщо процедура знайде шлях, напис буде змінено.*/
+                for (int i = 0; i < 11; i++)
+                {
+                    for (int j = 0; j < 11; j++)
+                    {
+                        dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;//Обнулюємо замальовані комірки.
+                    }
+                }
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox3.Items.Clear();
+                listBox4.Items.Clear();
+                minc = 999999;
+                maxc = -99999;
+                c = 0;
+                tupik = 0;
+                way = 0;
+                StreamWriter nul = new StreamWriter(results);//Чистимо файл із результатами перед застосуванням програми.
+                nul.Close();//Обнулення всіх лічильників перед початком роботи процедури  find.
+                find(i1, j1);
+                StreamWriter SW = new StreamWriter(results,true);//Створюємо засіб запису до файлу.
+                SW.WriteLine("Найкоротший шлях");
+                for (int i = 1; i <= minc; i++)
+                {
+                    listBox2.Items.Add("x:" + minl[i, 0] + " " + "y:" + minl[i, 1]);//Виводимо найкоротший шлях.
+                    SW.WriteLine("x:" + minl[i, 0] + " " + "y:" + minl[i, 1]);
+                }
+                SW.WriteLine("Найдовший шлях");
+                for (int i = 1; i <= maxc; i++)
+                {
+                    listBox3.Items.Add("x:" + maxl[i, 0] + " " + "y:" + maxl[i, 1]);//Виводимо найдовший шлях.
+                    SW.WriteLine("x:" + maxl[i, 0] + " " + "y:" + maxl[i, 1]);
+                }               
+                listBox4.Items.Add("Довжина найкоротшого виходу " + minc);
+                listBox4.Items.Add("Довжина найдовшого виходу " + maxc);
+                listBox4.Items.Add("Кількість виходів у лабіринті " + way);
+                listBox4.Items.Add("Кількість тупіків у лабіринті " + tupik);
+                SW.WriteLine("Довжина найкоротшого виходу " + minc);
+                SW.WriteLine("Довжина найдовшого виходу " + maxc);
+                SW.WriteLine("Кількість виходів у лабіринті " + way);
+                SW.WriteLine("Кількість тупіків у лабіринті " + tupik);
+                SW.Close();
+            }
+         }
+         private void button4_Click_1(object sender, EventArgs e)//Натиснувши четверту кнопку, закриваємо проект.
+        {  
+             Application.Exit();
+        }
+    }
+}
